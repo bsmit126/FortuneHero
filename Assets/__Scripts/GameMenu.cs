ce576b9f17674e11a3293b82ccccb4f008d00395
Sooperkewl <bsmit126@emich.edu>
2015-12-20 16:14:41 -0500

    FortuneHero Final Game
    Final game for Game Programming Class.

---
+++ Assets/__Scripts/GameMenu.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMenu : MonoBehaviour {
+
+	public void OnStartClick() {
+		Application.LoadLevel ("IntroScreen");
+	}
+
+    public void OnEnemyClick()
+    {
+        Application.LoadLevel("Enemy");
+    }
+
+	public void OnContinueClick() {
+		Application.LoadLevel ("Level_One");
+	}
+
+    public void OnBackClick()
+    {
+        Application.LoadLevel("GameStart");
+    }
+
+	public void OnExitClick() {
+		Application.Quit ();
+	}
+}
---
+++ Assets/__Scripts/TikimanController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TikimanController : MonoBehaviour {
+
+	public PlayerController player;
+
+	public int health;
+
+	public GameObject deathParticle;
+
+	private bool facingLeft;
+
+    public AudioClip explode;
+    private AudioSource source;
+
+    // Use this for initialization
+    void Start () {
+		player = FindObjectOfType<PlayerController> ();
+		facingLeft = true;
+		health = 50;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.transform.position.x < transform.position.x && !facingLeft) {
+			Flip ();
+		} else if (player.transform.position.x > transform.position.x && facingLeft) {
+			Flip();
+		}
+
+		if (health <= 0) {
+			Destroy (gameObject);
+			Instantiate (deathParticle, transform.position, transform.rotation);
+            source.PlayOneShot(explode, 0.29f);
+        }
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "RevolverBullet") {
+			health -= 10;
+		}
+	}
+
+	void Flip() {
+		facingLeft = !facingLeft;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
